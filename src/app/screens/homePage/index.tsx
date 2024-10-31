e9df05f48b3bddc116c052c73a3e97ef9981addd
Ozod26 <ozodfarid46@gmail.com>
2024-10-31 16:41:39 +0900

    feat: develop slice and selector usage

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,26 +1,40 @@
 import React, { useEffect } from "react";
 import Statistics from "./Statistics";
-import PopulerDishes from "./PopularDishes";
 import PopularDishes from "./PopularDishes";
-import NewDishes from "./NewDishes copy";
-import Advetisement from "./Advertisement";
+import NewDishes from "./NewDishes";
+import Advertisement from "./Advertisement";
 import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
 import "../../../css/home.css";
 
-export default function HomePage() {
-  // Selector: Store => Data
+import { useDispatch, useSelector } from "react-redux";
+import { Dispatch } from "@reduxjs/toolkit";
+import { createSelector } from "reselect";
+import { setPopularDishes } from "./slice";
+import { retrievePopularDishes } from "./selector";
+import { Product } from "../../../lib/types/product";
 
-  useEffect(() => {
-    // Backend server data fetch => DATA
-    // Slice: Data => Store
-  }, []);
+const actionDispatch = (dispatch: Dispatch) => ({
+  setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
+});
+
+const popularDishesRetriever = createSelector(
+  retrievePopularDishes,
+  (popularDishes) => ({ popularDishes })
+);
+
+export default function HomePage() {
+  const { setPopularDishes } = actionDispatch(useDispatch());
+  const { popularDishes } = useSelector(popularDishesRetriever);
+
+  useEffect(() => {}, []);
+
   return (
-    <div className={"homepage"}>
+    <div className="homepage">
       <Statistics />
       <PopularDishes />
       <NewDishes />
-      <Advetisement />
+      <Advertisement />
       <ActiveUsers />
       <Events />
     </div>
